d03a23061499888de5bd0eab4ba1d484e4123ae3
MCProtein <chss3339@gmail.com>
2025-07-22 15:14:19 +0900

    init

---
+++ src/dto.d.ts
@@ -0,0 +1,10 @@
+export declare class A {
+    test(): string;
+}
+export type AType = {
+    name: string;
+};
+export interface AInterface {
+    name: string;
+}
+//# sourceMappingURL=dto.d.ts.map
---
+++ src/dto.ts
@@ -0,0 +1,13 @@
+export class A {
+  test() {
+    return 'a'
+  }
+}
+
+export type AType = {
+  name: string
+}
+
+export interface AInterface {
+  name: string
+}
